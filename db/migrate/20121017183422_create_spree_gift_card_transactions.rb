7b8ac9c796ec3451cea2cc060ab77e2f41141cf4
Robert Balousek <rbalousek@hotmail.com>
2013-08-29 18:10:03 -0400

    Update 20121017183422_create_spree_gift_card_transactions.rb

--- db/migrate/20121017183422_create_spree_gift_card_transactions.rb
+++ db/migrate/20121017183422_create_spree_gift_card_transactions.rb
@@ -1,7 +1,7 @@
 class CreateSpreeGiftCardTransactions < ActiveRecord::Migration
   def change
     create_table :spree_gift_card_transactions do |t|
-      t.decimal :amount, scale: 2, precision: 15
+      t.decimal :amount, :scale => 2, :precision => 10
       t.belongs_to :gift_card
       t.belongs_to :order
       t.timestamps
